d0e788bbb4b84c0b3de4ce2fccf5ed7e732fd1ba
Tigran542 <tikogrig7@gmail.com>
2023-08-03 18:12:36 +0400

    PatrolState script

---
+++ Assets/Scripts/Enemy/States/PatrolState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolState : BaseState
+{
+
+    public int waypointIndex;
+    public override void Enter()
+    {
+    }
+    public override void Perform()
+    {
+    }
+    public override void Exit()
+    {
+    }
+
+    public void PatrolCycle()
+    {
+        //implement our patrol logic
+    }
+}
